18d4f40718dbd8ae9bc94fa959bf3c6daa61674d
Gabriele Buondonno <gbuondon@laas.fr>
2019-01-22 18:08:50 +0100

    velocity-based joint admittance control

---
+++ python/sot_talos_balance/test/test_admittance_single_joint_velocity_based.py
@@ -0,0 +1,16 @@
+from sot_talos_balance.utils.run_test_utils import run_test, runCommandClient, evalCommandClient
+from time import sleep
+
+run_test('appli_admittance_single_joint_velocity_based.py')
+
+sleep(1.0)
+runCommandClient('robot.admittance_control.tauDes.value = [target]')
+runCommandClient('robot.sot.push(robot.taskJoint.task.name)')
+
+sleep(5.0)
+des_tau = evalCommandClient('target')
+tau = evalCommandClient('robot.device.ptorque.value[JOINT]')
+
+print("Desired torque: %f" % des_tau)
+print("Current torque: %f" % tau)
+
