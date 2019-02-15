d5bbd2eb854888b70656e38020172d868c505411
fbailly <fbailly@laas.fr>
2019-02-15 14:21:49 +0100

    [added plot_positions bash script]

---
+++ scripts/plot_positions.sh
@@ -0,0 +1,3 @@
+rqt_plot /sot/torso_2_link/position/data[0] /sot/base_estimator/q/data[0]&
+rqt_plot /sot/torso_2_link/position/data[1] /sot/base_estimator/q/data[1] &
+rqt_plot /sot/torso_2_link/position/data[2] /sot/base_estimator/q/data[2]&
