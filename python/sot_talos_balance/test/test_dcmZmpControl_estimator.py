cc6e40f0ff2ec03dad1f5910882253605c94c793
Gabriele Buondonno <gbuondon@laas.fr>
2019-04-08 16:54:25 +0200

    [test_dcmZmpControl_estimator] Added F/T calibration and control manager

--- python/sot_talos_balance/test/test_dcmZmpControl_estimator.py
+++ python/sot_talos_balance/test/test_dcmZmpControl_estimator.py
@@ -1,31 +1,23 @@
 '''Test CoM admittance control as described in paper.'''
-from sot_talos_balance.utils.run_test_utils import run_test, runCommandClient, evalCommandClient
+from sot_talos_balance.utils.run_test_utils import *
 from time import sleep
-
-from sot_talos_balance.utils.gazebo_utils import apply_force
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 run_test('appli_dcmZmpControl_estimator.py')
 
-sleep(5.0)
+run_ft_calibration('robot.ftc')
+raw_input("Wait before running the test")
 
 # Connect ZMP reference and reset controllers
 print('Connect ZMP reference')
+runCommandClient('plug(robot.zmp_estimator.emergencyStop,robot.cm.emergencyStop_zmp)')
 runCommandClient('plug(robot.dcm_control.zmpRef,robot.com_admittance_control.zmpDes)')
 runCommandClient('robot.com_admittance_control.setState(comDes,[0.0,0.0,0.0])')
 runCommandClient('robot.com_admittance_control.Kp.value = Kp_adm')
 runCommandClient('robot.dcm_control.resetDcmIntegralError()')
 runCommandClient('robot.dcm_control.Ki.value = Ki_dcm')
-
-#sleep(5.0)
-
-#print('Kick the robot...')
-#apply_force([-1000.0,0,0],0.01)
-#print('... kick!')
-
-#sleep(5.0)
 
 sleep(30.0)
 
