a8a20a6f072e89ac4d0fc6b264ec0d978537c0a3
Gabriele Buondonno <gbuondon@laas.fr>
2019-04-10 16:04:28 +0200

    [test_dcm_estimator] Rework

--- python/sot_talos_balance/test/appli_dcm_estimator.py
+++ python/sot_talos_balance/test/appli_dcm_estimator.py
@@ -1,26 +1,91 @@
-from sot_talos_balance.create_entities_utils           import *
-from sot_talos_balance.utils.plot_utils                import *
+from sot_talos_balance.create_entities_utils import *
 import sot_talos_balance.talos.parameter_server_conf   as param_server_conf
+import sot_talos_balance.talos.control_manager_conf    as cm_conf
 import sot_talos_balance.talos.base_estimator_conf     as base_estimator_conf
-from dynamic_graph.sot.core.meta_tasks_kine            import MetaTaskKine6d, MetaTaskKineCom, gotoNd
-from dynamic_graph.sot.core.matrix_util                import matrixToTuple
-from dynamic_graph                                     import plug
-from dynamic_graph.sot.core                            import SOT
-from time                                              import sleep
-from dynamic_graph.ros                                 import RosSubscribe
-import os
-import numpy                                           as np
-from dynamic_graph.ros                                 import RosPublish
+import sot_talos_balance.talos.ft_calibration_conf     as ft_conf
+from dynamic_graph.sot.core.meta_tasks_kine import MetaTaskKine6d, MetaTaskKineCom, gotoNd
+from dynamic_graph import plug
+from dynamic_graph.sot.core import SOT
+from math import sqrt
 
+from dynamic_graph.tracer_real_time import TracerRealTime
+
+from dynamic_graph.sot.dynamics_pinocchio import DynamicPinocchio
 
 robot.timeStep = robot.device.getTimeStep()
 dt = robot.timeStep;
+
+# -------------------------- DESIRED TRAJECTORY --------------------------
+
 robot.comTrajGen = create_com_trajectory_generator(dt,robot);
-# --- COM
-robot.taskCom = MetaTaskKineCom(robot.dynamic)
-robot.dynamic.com.recompute(0)
-robot.taskCom.featureDes.errorIN.value = robot.dynamic.com.value
-robot.taskCom.task.controlGain.value = 10
+
+# --- Pendulum parameters
+robot_name='robot'
+robotDim = robot.dynamic.getDimension()
+mass = robot.dynamic.data.mass[0]
+h = robot.dynamic.com.value[2]
+g = 9.81
+omega = sqrt(g/h)
+
+# -------------------------- ESTIMATION --------------------------
+
+# --- Base Estimation
+robot.param_server            = create_parameter_server(param_server_conf,dt)
+robot.device_filters          = create_device_filters(robot, dt)
+robot.imu_filters             = create_imu_filters(robot, dt)
+robot.base_estimator          = create_base_estimator(robot, dt, base_estimator_conf)
+# robot.be_filters              = create_be_filters(robot, dt)
+
+# --- Conversion
+e2q = EulerToQuat('e2q')
+plug(robot.base_estimator.q,e2q.euler)
+robot.e2q = e2q
+
+# --- Kinematic computations
+robot.rdynamic = DynamicPinocchio("real_dynamics")
+robot.rdynamic.setModel(robot.dynamic.model)
+robot.rdynamic.setData(robot.rdynamic.model.createData())
+plug(robot.base_estimator.q,robot.rdynamic.position)
+robot.rdynamic.velocity.value = [0.0]*robotDim
+robot.rdynamic.acceleration.value = [0.0]*robotDim
+
+# --- CoM Estimation
+cdc_estimator = DcmEstimator('cdc_estimator')
+cdc_estimator.init(dt, robot_name)
+plug(robot.e2q.quaternion, cdc_estimator.q)
+plug(robot.base_estimator.v, cdc_estimator.v)
+robot.cdc_estimator = cdc_estimator
+
+# --- DCM Estimation
+estimator = DummyDcmEstimator("dummy")
+estimator.omega.value = omega
+estimator.mass.value = 1.0
+plug(robot.cdc_estimator.c, estimator.com)
+plug(robot.cdc_estimator.dc,estimator.momenta)
+estimator.init()
+robot.estimator = estimator
+
+# --- Force calibration
+robot.ftc = create_ft_calibrator(robot,ft_conf)
+
+# --- ZMP estimation (disconnected)
+zmp_estimator = SimpleZmpEstimator("zmpEst")
+robot.rdynamic.createOpPoint('sole_LF','left_sole_link')
+robot.rdynamic.createOpPoint('sole_RF','right_sole_link')
+plug(robot.rdynamic.sole_LF,zmp_estimator.poseLeft)
+plug(robot.rdynamic.sole_RF,zmp_estimator.poseRight)
+plug(robot.ftc.left_foot_force_out,zmp_estimator.wrenchLeft)
+plug(robot.ftc.right_foot_force_out,zmp_estimator.wrenchRight)
+zmp_estimator.init()
+robot.zmp_estimator = zmp_estimator
+
+# --- Control Manager
+robot.cm = create_ctrl_manager(cm_conf, dt, robot_name='robot')
+robot.cm.addCtrlMode('sot_input')
+robot.cm.setCtrlMode('all','sot_input')
+robot.cm.addEmergencyStopSIN('zmp')
+
+# -------------------------- SOT CONTROL --------------------------
 
 # --- CONTACTS
 #define contactLF and contactRF
@@ -36,47 +101,53 @@ robot.contactRF.gain.setConstant(100)
 robot.contactRF.keep()
 locals()['contactRF'] = robot.contactRF
 
-# --- SOT
+# --- COM
+robot.taskCom = MetaTaskKineCom(robot.dynamic)
+robot.dynamic.com.recompute(0)
+robot.taskCom.featureDes.errorIN.value = robot.dynamic.com.value
+robot.taskCom.task.controlGain.value = 10
+
+# --- SOT solver
 robot.sot = SOT('sot')
 robot.sot.setSize(robot.dynamic.getDimension())
-plug(robot.sot.control,robot.device.control)
-plug(robot.comTrajGen.x,    robot.taskCom.featureDes.errorIN);
+
+# --- Plug SOT control to device through control manager
+plug(robot.sot.control,robot.cm.ctrl_sot_input)
+plug(robot.cm.u_safe,robot.device.control)
+
+plug(robot.comTrajGen.x,    robot.taskCom.featureDes.errorIN)
 
 robot.sot.push(robot.contactRF.task.name)
-robot.sot.push(robot.taskCom.task.name)
 robot.sot.push(robot.contactLF.task.name)
+robot.sot.push(robot.taskCom.task.name)
 robot.device.control.recompute(0)
 
-# --- ESTIMATION
-robot.param_server            = create_parameter_server(param_server_conf,dt)
-# robot.imu_offset_compensation = create_imu_offset_compensation(robot, dt)
-robot.device_filters          = create_device_filters(robot, dt)
-robot.imu_filters             = create_imu_filters(robot, dt)
-robot.base_estimator          = create_base_estimator(robot, dt, base_estimator_conf) 
-robot.be_filters              = create_be_filters(robot, dt)
+# --- Fix robot.dynamic inputs
+plug(robot.device.velocity,robot.dynamic.velocity)
+from dynamic_graph.sot.core import Derivator_of_Vector
+robot.dvdt = Derivator_of_Vector("dv_dt")
+robot.dvdt.dt.value = dt
+plug(robot.device.velocity,robot.dvdt.sin)
+plug(robot.dvdt.sout,robot.dynamic.acceleration)
 
-robot_name='robot'
-e2q = EulerToQuat('e2q')
-plug(robot.base_estimator.q,e2q.euler)
-robot.e2q = e2q
-dcm_estimator = DcmEstimator('dcm_estimator')
-dcm_estimator.init(dt, robot_name)
-plug(robot.e2q.quaternion, dcm_estimator.q)
-plug(robot.base_estimator.v, dcm_estimator.v)
-robot.dcm_estimator = dcm_estimator
+# -------------------------- PLOTS --------------------------
 
 # --- ROS PUBLISHER
-robot.publisher = create_rospublish(robot, 'robot_publisher')
-create_topic(robot.publisher, robot.base_estimator, 'q', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.base_estimator, 'q_imu', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.device_filters.gyro_filter, 'x_filtered', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.device_filters.acc_filter, 'x_filtered', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.dcm_estimator, 'c', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.dcm_estimator, 'dc', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.base_estimator, 'v', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.base_estimator, 'v_gyr', robot = robot, data_type='vector')
+robot.publisher = create_rospublish(robot, 'robot_publisher')        
 
-create_topic(robot.publisher, robot.dynamic, 'com', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.device, 'state', robot = robot, data_type='vector')
-create_topic(robot.publisher, robot.device, 'velocity', robot = robot, data_type='vector')
+create_topic(robot.publisher, robot.zmp_estimator, 'zmp', robot = robot, data_type='vector')              # estimated ZMP
+create_topic(robot.publisher, robot.zmp_estimator, 'emergencyStop', robot = robot, data_type='boolean')   # ZMP emergency stop
+create_topic(robot.publisher, robot.dynamic, 'com', robot = robot, data_type='vector')                    # SOT CoM
+create_topic(robot.publisher, robot.dynamic, 'zmp', robot = robot, data_type='vector')                    # SOT ZMP
+create_topic(robot.publisher, robot.device, 'forceLLEG', robot = robot, data_type='vector')               # force on left foot
+create_topic(robot.publisher, robot.device, 'forceRLEG', robot = robot, data_type='vector')               # force on right foot
+
+#create_topic(robot.publisher, robot.device, 'forceLLEG', robot = robot, data_type='vector')               # measured left wrench
+#create_topic(robot.publisher, robot.device, 'forceRLEG', robot = robot, data_type='vector')               # measured right wrench
+
+#create_topic(robot.publisher, robot.device_filters.ft_LF_filter, 'x_filtered', robot = robot, data_type='vector') # filtered left wrench
+#create_topic(robot.publisher, robot.device_filters.ft_RF_filter, 'x_filtered', robot = robot, data_type='vector') # filtered right wrench
+
+create_topic(robot.publisher, robot.ftc, 'left_foot_force_out', robot = robot, data_type='vector')  # calibrated left wrench
+create_topic(robot.publisher, robot.ftc, 'right_foot_force_out', robot = robot, data_type='vector') # calibrated right wrench
 
--- python/sot_talos_balance/test/test_dcm_estimator.py
+++ python/sot_talos_balance/test/test_dcm_estimator.py
@@ -1,37 +1,21 @@
-from sot_talos_balance.utils.run_test_utils import run_test, runCommandClient, evalCommandClient
-from sot_talos_balance.utils.gazebo_utils   import is_gazebo_present, GazeboLinkStatePublisher
-from time                                   import sleep
-import matplotlib.pyplot                    as plt
-import numpy                                as np
+from sot_talos_balance.utils.run_test_utils import *
+from time import sleep
 
-pub_base = None
-if is_gazebo_present():
-    pub_base = GazeboLinkStatePublisher('base_link',1000,local_frame = False)
-    print("Starting Gazebo link state publisher...")
-    pub_base.start()
-    print("Gazebo link state publisher started")
-
-raw_input("Wait before running the test")
+import matplotlib.pyplot as plt
+import numpy as np
 
 run_test('appli_dcm_estimator.py')
 
+run_ft_calibration('robot.ftc')
+raw_input("Wait before running the test")
+
+# plug ZMP emergency signal
+runCommandClient('plug(robot.zmp_estimator.emergencyStop,robot.cm.emergencyStop_zmp)')
 sleep(2.0)
-runCommandClient('robot.comTrajGen.move(1,-0.025,3.0)')
-sleep(10.0)
-runCommandClient('robot.comTrajGen.startSinusoid(1,0.05,1.0)')
-sleep(5.0)
+runCommandClient('robot.comTrajGen.move(1,-0.025,1.0)')
+sleep(20.0)
+runCommandClient('robot.comTrajGen.startSinusoid(1,0.05,2.0)')
+sleep(20.0)
 
-# # --- DUMP SIGNALS
-# runCommandClient("dump_sot_sigs(robot,[[robot.base_estimator,'q','q_imu'],[robot.subscriber,'position','velocity']],5.)")
-# sleep(6.)
+raw_input("Wait before leaving the simulation")
 
-
-# plt.ion()
-
-raw_input("Wait before leaving the test")
-
-if pub_base is not None:
-    print("Stopping Gazebo link state publisher...")
-    pub_base.stop()
-    sleep(0.1)
-    print("Gazebo link state publisher stopped")
