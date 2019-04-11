eb5b07e2cb4959d38359952dfc351aa815a76800
Fanny Risbourg <frisbourg@laas.fr>
2019-04-11 17:29:42 +0200

    [EE Admittance] Tracer added

--- python/sot_talos_balance/test/appli_admittance_end_effector.py
+++ python/sot_talos_balance/test/appli_admittance_end_effector.py
@@ -9,27 +9,33 @@ from dynamic_graph.ros import RosSubscribe, RosPublish
 import numpy as np
 import math
 
+from dynamic_graph.tracer_real_time import TracerRealTime
+from sot_talos_balance.create_entities_utils import addTrace, dump_tracer
+
 robot.timeStep = robot.device.getTimeStep()
+
+# --- SET INITIAL CONFIGURATION ------------------------------------------------
 
 q =  [0., 0., 1.018213, 0., 0., 0.]                                  #Base
 q += [0., 0., -0.411354, 0.859395, -0.448041, -0.001708]             #Left Leg
 q += [0., 0., -0.411354, 0.859395, -0.448041, -0.001708]             #Right Leg
 q += [0.0 ,  0.006761]                                               #Chest
 q += [ 0.25847, 0.173046, -0.0002, -0.525366, 0., 0., 0.1, -0.005]   #Left Arm
-q += [-0.25847, -0.0, 0.19, -1.61, 0., 0., 0.1, -0.005]              #Right Arm
+q += [-0.25847, -0.173046, 0.0002, -0.525366, 0., 0., 0.1, -0.005]   #Right Arm
+# q += [-0.25847, -0.0, 0.19, -1.61, 0., 0., 0.1, -0.005]              #Right Arm
 q += [0.,  0.]                                                       #Head
 robot.device.set(q)
 
-# --- PARAM_SERVER ---
+# --- CREATE ENTITIES ----------------------------------------------------------
+
 robot.param_server = create_parameter_server(paramServerConfig, robot.timeStep)
 robot.device_filters = create_device_filters(robot, robot.timeStep)
 robot.imu_filters = create_imu_filters(robot, robot.timeStep)
 robot.baseEstimator = create_base_estimator(robot, robot.timeStep, baseEstimatorConf)
-
-# --- ADMITTANCE CONTROLLER ---
 robot.controller = create_end_effector_admittance_controller(robot, 'rightWrist')
 
-# --- HAND TASK ---
+# --- HAND TASK ----------------------------------------------------------------
+
 taskRightHand = MetaTaskKine6d('rh', robot.dynamic, 'rh', 'arm_right_7_joint')
 handMgrip = np.eye(4)
 handMgrip[0:3, 3] = (0.1, 0, 0)
@@ -43,7 +49,8 @@ taskRightHand.feature.velocity.value = [0., 0., 0., 0., 0., 0.]
 taskRightHand.featureDes.position.value = np.eye(4)
 plug(robot.controller.dq, taskRightHand.featureDes.velocity)
 
-# --- BASE TASK ---
+# --- BASE TASK ----------------------------------------------------------------
+
 taskWaist = MetaTaskKine6d('taskWaist',robot.dynamic,'WT',robot.OperationalPointsMap['waist'])
 taskWaist.feature.frame('desired')
 taskWaist.gain.setConstant(300)
@@ -51,7 +58,8 @@ taskWaist.keep()
 taskWaist.feature.selec.value = '111111'
 locals()['taskWaist'] = taskWaist
 
-# --- POSTURE TASK ---
+# --- POSTURE TASK -------------------------------------------------------------
+
 robot.taskPosture = Task('task_posture')
 robot.taskPosture.controlGain.value = 100.0
 robot.taskPosture.feature = FeaturePosture('feature_posture')
@@ -82,12 +90,14 @@ robot.sot = SOT('sot')
 robot.sot.setSize(robot.dynamic.getDimension())
 plug(robot.sot.control, robot.device.control)
 
-# --- PUSH THE TASKS ---
+# --- PUSH THE TASKS -----------------------------------------------------------
+
 robot.sot.push(robot.taskPosture.name)
 robot.sot.push(taskRightHand.task.name)
 robot.sot.push(taskWaist.task.name)
 
-# # --- ROS PUBLISHER
+# # --- ROS PUBLISHER ----------------------------------------------------------
+
 robot.publisher = create_rospublish(robot, 'robot_publisher')
 create_topic(robot.publisher, robot.controller, 'w_force', robot=robot, data_type='vector')
 create_topic(robot.publisher, robot.controller, 'force', robot=robot, data_type='vector')
@@ -102,3 +112,15 @@ robot.subscriber.add("vector", "force", "/sot/controller/force")
 robot.subscriber.add("vector", "dq", "/sot/controller/dq")
 robot.subscriber.add("vector", "w_dq", "/sot/controller/w_dq")
 robot.subscriber.add("vector", "w_forceDes", "/sot/controller/w_forceDes")
+
+# --- TRACER  ------------------------------------------------------------------
+
+robot.tracer = TracerRealTime("force_tracer")
+robot.tracer.setBufferSize(80*(2**20))
+robot.tracer.open('/tmp','dg_','.dat')
+robot.device.after.addSignal('{0}.triger'.format(robot.tracer.name))
+
+addTrace(robot.tracer, robot.controller, 'force')
+addTrace(robot.tracer, robot.controller, 'w_force')
+
+robot.tracer.start()
--- python/sot_talos_balance/test/test_admittance_end_effector.py
+++ python/sot_talos_balance/test/test_admittance_end_effector.py
@@ -1,4 +1,34 @@
 from sot_talos_balance.utils.run_test_utils import run_test, runCommandClient, evalCommandClient
 from time import sleep
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 run_test('appli_admittance_end_effector.py')
+
+sleep(50.0)
+
+runCommandClient('dump_tracer(robot.tracer)')
+
+# --- DISPLAY
+force_data = np.loadtxt('/tmp/dg_' + evalCommandClient('robot.controller.name') + '-force.dat')
+w_force_data = np.loadtxt('/tmp/dg_' + evalCommandClient('robot.controller.name') + '-w_force.dat')
+
+plt.ion()
+
+plt.figure()
+plt.plot(force_data[:,1],'b-')
+plt.plot(force_data[:,2],'r-')
+plt.plot(force_data[:,3],'g-')
+plt.title('force')
+plt.legend(['force x', 'force y', 'force z'])
+
+plt.figure()
+plt.plot(w_force_data[:,1],'b-')
+plt.plot(w_force_data[:,2],'r-')
+plt.plot(w_force_data[:,3],'g-')
+plt.title('w_force')
+plt.legend(['w_force x', 'w_force y', 'w_force z'])
+
+raw_input("Wait before leaving the simulation")
+
