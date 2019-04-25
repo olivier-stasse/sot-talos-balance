olivier-stasse/sot-talos-balance

commit 37db1274c7242462041f5ee14bf5ae45df68276b

Prepare test_dcmZmpControl_file

src/dummy-walking-pattern-generator.cpp
@@ -36,7 +36,7 @@ namespace dynamicgraph
 //Size to be aligned                                         "-------------------------------------------------------"
 #define PROFILE_DUMMYWALKINGPATTERNGENERATOR_DCM_COMPUTATION "DummyWalkingPatternGenerator: dcm computation          "
 
-#define INPUT_SIGNALS     m_omegaSIN << m_footLeftSIN << m_footRightSIN << m_waistSIN << m_footPositionLeftSIN << m_footPositionRightSIN << m_comSIN << m_vcomSIN << m_acomSIN << m_referenceFrameSIN
+#define INPUT_SIGNALS     m_omegaSIN << m_footLeftSIN << m_footRightSIN << m_waistSIN << m_footPositionLeftSIN << m_footPositionRightSIN << m_comSIN << m_vcomSIN << m_acomSIN << m_zmpSIN << m_referenceFrameSIN
 
 #define INNER_SIGNALS m_rfSINNER
 
@@ -64,13 +64,14 @@ namespace dynamicgraph
                       , CONSTRUCT_SIGNAL_IN(com,  dynamicgraph::Vector)
                       , CONSTRUCT_SIGNAL_IN(vcom, dynamicgraph::Vector)
                       , CONSTRUCT_SIGNAL_IN(acom, dynamicgraph::Vector)
+                      , CONSTRUCT_SIGNAL_IN(zmp, dynamicgraph::Vector)
                       , CONSTRUCT_SIGNAL_IN(referenceFrame, MatrixHomogeneous)
                       , CONSTRUCT_SIGNAL_INNER(rf, MatrixHomogeneous, m_referenceFrameSIN)
                       , CONSTRUCT_SIGNAL_OUT(comDes,  dynamicgraph::Vector, m_comSIN  << m_rfSINNER)
                       , CONSTRUCT_SIGNAL_OUT(vcomDes, dynamicgraph::Vector, m_vcomSIN << m_rfSINNER)
                       , CONSTRUCT_SIGNAL_OUT(acomDes, dynamicgraph::Vector, m_acomSIN << m_rfSINNER)
                       , CONSTRUCT_SIGNAL_OUT(dcmDes,  dynamicgraph::Vector, m_omegaSIN << m_comDesSOUT << m_vcomDesSOUT)
-                      , CONSTRUCT_SIGNAL_OUT(zmpDes,  dynamicgraph::Vector, m_omegaSIN << m_comDesSOUT << m_acomDesSOUT)
+                      , CONSTRUCT_SIGNAL_OUT(zmpDes,  dynamicgraph::Vector, m_omegaSIN << m_comDesSOUT << m_acomDesSOUT << m_rfSINNER << m_zmpSIN)
                       , CONSTRUCT_SIGNAL_OUT(footLeftDes,  MatrixHomogeneous, m_footLeftSIN  << m_footPositionLeftSIN  << m_rfSINNER)
                       , CONSTRUCT_SIGNAL_OUT(footRightDes, MatrixHomogeneous, m_footRightSIN << m_footPositionRightSIN << m_rfSINNER)
                       , CONSTRUCT_SIGNAL_OUT(waistDes, MatrixHomogeneous, m_waistSIN << m_rfSINNER)
@@ -204,12 +205,29 @@ namespace dynamicgraph
           return s;
         }
 
-        const double & omega = m_omegaSIN(iter);
-        const Vector & comDes = m_comDesSOUT(iter);
-        const Vector & acomDes = m_acomDesSOUT(iter);
+        Vector zmpDes;
 
-        Vector zmpDes = comDes - acomDes/(omega*omega);
-        zmpDes[2] = 0.0;
+        if(m_zmpSIN.isPlugged())
+        {
+          const Vector & zmp = m_zmpSIN(iter);
+          const MatrixHomogeneous & referenceFrame = m_rfSINNER(iter);
+
+          zmpDes.resize(3);
+          zmpDes[0] = zmp[0];
+          zmpDes[1] = zmp[1];
+          zmpDes[2] = zmp.size()>2 ? zmp[2] : 0.;
+
+          zmpDes = actInv(referenceFrame, zmpDes);
+        }
+        else
+        {
+          const double & omega = m_omegaSIN(iter);
+          const Vector & comDes = m_comDesSOUT(iter);
+          const Vector & acomDes = m_acomDesSOUT(iter);
+
+          Vector zmpDes = comDes - acomDes/(omega*omega);
+          zmpDes[2] = 0.0;
+        }
 
         s = zmpDes;
 

include/sot/talos_balance/dummy-walking-pattern-generator.hh
@@ -79,6 +79,8 @@ namespace dynamicgraph {
         DECLARE_SIGNAL_IN(vcom, dynamicgraph::Vector);
         DECLARE_SIGNAL_IN(acom, dynamicgraph::Vector);
 
+        DECLARE_SIGNAL_IN(zmp,  dynamicgraph::Vector);
+
         DECLARE_SIGNAL_IN(referenceFrame, MatrixHomogeneous);
 
         DECLARE_SIGNAL_INNER(rf, MatrixHomogeneous);

python/sot_talos_balance/create_entities_utils.py
@@ -80,6 +80,15 @@ def create_com_trajectory_generator(dt, robot):
     comTrajGen.init(dt, 3)
     return comTrajGen
 
+def create_zmp_trajectory_generator(dt, robot):
+    comTrajGen = NdTrajectoryGenerator("zmpTrajGen")
+    zmp = list(robot.dynamic.com.value)
+    zmp[2] = 0.0
+    comTrajGen.initial_value.value = zmp
+    comTrajGen.trigger.value = 1.0
+    comTrajGen.init(dt, 3)
+    return comTrajGen
+
 def create_position_trajectory_generator(dt, robot, signal_name):
     trajGen = NdTrajectoryGenerator(signal_name+"TrajGen")
 

python/sot_talos_balance/test/appli_dcmZmpControl_file.py
@@ -1,0 +1,377 @@
+from sot_talos_balance.create_entities_utils import *
+import sot_talos_balance.talos.parameter_server_conf   as param_server_conf
+import sot_talos_balance.talos.control_manager_conf    as cm_conf
+import sot_talos_balance.talos.base_estimator_conf     as base_estimator_conf
+import sot_talos_balance.talos.ft_calibration_conf     as ft_conf
+from dynamic_graph.sot.core.meta_tasks_kine import MetaTaskKine6d, MetaTaskKineCom, gotoNd
+from dynamic_graph.sot.core import Task, FeaturePosture
+from dynamic_graph.sot.core.matrix_util import matrixToTuple
+from dynamic_graph import plug
+from dynamic_graph.sot.core import SOT
+from math import sqrt
+import numpy as np
+
+from dynamic_graph.tracer_real_time import TracerRealTime
+
+from dynamic_graph.sot.dynamics_pinocchio import DynamicPinocchio
+
+robot.timeStep = robot.device.getTimeStep()
+dt = robot.timeStep
+
+# --- Pendulum parameters
+robot_name='robot'
+robot.dynamic.com.recompute(0)
+robotDim = robot.dynamic.getDimension()
+mass = robot.dynamic.data.mass[0]
+h = robot.dynamic.com.value[2]
+g = 9.81
+omega = sqrt(g/h)
+
+# --- Parameter server
+robot.param_server = create_parameter_server(param_server_conf,dt)
+
+# --- Initial feet and waist
+robot.dynamic.createOpPoint('LF',robot.OperationalPointsMap['left-ankle'])
+robot.dynamic.createOpPoint('RF',robot.OperationalPointsMap['right-ankle'])
+robot.dynamic.createOpPoint('WT',robot.OperationalPointsMap['waist'])
+robot.dynamic.LF.recompute(0)
+robot.dynamic.RF.recompute(0)
+robot.dynamic.WT.recompute(0)
+
+# -------------------------- DESIRED TRAJECTORY --------------------------
+
+# --- Trajectory generators
+robot.comTrajGen = create_com_trajectory_generator(dt,robot)
+robot.lfPosTrajGen  = create_position_trajectory_generator(dt, robot, 'LF')
+robot.rfPosTrajGen  = create_position_trajectory_generator(dt, robot, 'RF')
+robot.zmpTrajGen = create_zmp_trajectory_generator(dt,robot)
+
+# --- Interface with controller entities
+
+wp = DummyWalkingPatternGenerator('dummy_wp')
+wp.init()
+wp.omega.value = omega
+wp.waist.value = robot.dynamic.WT.value          # wait receives a full homogeneous matrix, but only the rotational part is controlled
+wp.footLeft.value = robot.dynamic.LF.value
+wp.footRight.value = robot.dynamic.RF.value
+plug(robot.lfPosTrajGen.x, wp.footPositionLeft)  # if given, footPositionLeft overrides the translation in footLeft
+plug(robot.rfPosTrajGen.x, wp.footPositionRight) # if given, footPositionRight overrides the translation in footRight
+plug(robot.comTrajGen.x, wp.com)
+plug(robot.comTrajGen.dx, wp.vcom)
+plug(robot.comTrajGen.ddx, wp.acom)
+plug(robot.zmpTrajGen.x, wp.zmp)
+robot.wp = wp
+
+# --- Compute the values to use them in initialization
+robot.wp.comDes.recompute(0)
+robot.wp.dcmDes.recompute(0)
+robot.wp.zmpDes.recompute(0)
+
+# -------------------------- ESTIMATION --------------------------
+
+# --- Base Estimation
+robot.device_filters          = create_device_filters(robot, dt)
+robot.imu_filters             = create_imu_filters(robot, dt)
+robot.base_estimator          = create_base_estimator(robot, dt, base_estimator_conf)
+# robot.be_filters              = create_be_filters(robot, dt)
+
+# --- Reference frame
+
+rf = SimpleReferenceFrame('rf')
+rf.init(robot_name)
+plug(robot.dynamic.LF, rf.footLeft)
+plug(robot.dynamic.RF, rf.footRight)
+robot.rf = rf
+
+# --- State transformation
+stf = StateTransformation("stf")
+stf.init()
+plug(robot.rf.referenceFrame,stf.referenceFrame)
+plug(robot.base_estimator.q,stf.q_in)
+plug(robot.base_estimator.v,stf.v_in)
+robot.stf = stf
+
+# --- Conversion
+e2q = EulerToQuat('e2q')
+plug(robot.stf.q,e2q.euler)
+robot.e2q = e2q
+
+# --- Kinematic computations
+robot.rdynamic = DynamicPinocchio("real_dynamics")
+robot.rdynamic.setModel(robot.dynamic.model)
+robot.rdynamic.setData(robot.rdynamic.model.createData())
+plug(robot.stf.q,robot.rdynamic.position)
+robot.rdynamic.velocity.value = [0.0]*robotDim
+robot.rdynamic.acceleration.value = [0.0]*robotDim
+
+# --- CoM Estimation
+cdc_estimator = DcmEstimator('cdc_estimator')
+cdc_estimator.init(dt, robot_name)
+plug(robot.e2q.quaternion, cdc_estimator.q)
+plug(robot.stf.v, cdc_estimator.v)
+robot.cdc_estimator = cdc_estimator
+
+# --- DCM Estimation
+estimator = DummyDcmEstimator("dummy")
+estimator.omega.value = omega
+estimator.mass.value = 1.0
+plug(robot.cdc_estimator.c, estimator.com)
+plug(robot.cdc_estimator.dc,estimator.momenta)
+estimator.init()
+robot.estimator = estimator
+
+# --- Force calibration
+robot.ftc = create_ft_calibrator(robot,ft_conf)
+
+# --- ZMP estimation
+zmp_estimator = SimpleZmpEstimator("zmpEst")
+robot.rdynamic.createOpPoint('sole_LF','left_sole_link')
+robot.rdynamic.createOpPoint('sole_RF','right_sole_link')
+plug(robot.rdynamic.sole_LF,zmp_estimator.poseLeft)
+plug(robot.rdynamic.sole_RF,zmp_estimator.poseRight)
+plug(robot.ftc.left_foot_force_out,zmp_estimator.wrenchLeft)
+plug(robot.ftc.right_foot_force_out,zmp_estimator.wrenchRight)
+zmp_estimator.init()
+robot.zmp_estimator = zmp_estimator
+
+# -------------------------- ADMITTANCE CONTROL --------------------------
+
+# --- DCM controller
+Kp_dcm = [3.0,3.0,3.0]
+Ki_dcm = [0.0,0.0,0.0] # zero (to be set later)
+gamma_dcm = 0.2
+
+dcm_controller = DcmController("dcmCtrl")
+
+dcm_controller.Kp.value = Kp_dcm
+dcm_controller.Ki.value = Ki_dcm
+dcm_controller.decayFactor.value = gamma_dcm
+dcm_controller.mass.value = mass
+dcm_controller.omega.value = omega
+
+plug(robot.cdc_estimator.c,dcm_controller.com)
+plug(robot.estimator.dcm,dcm_controller.dcm)
+
+plug(robot.wp.zmpDes, dcm_controller.zmpDes)
+plug(robot.wp.dcmDes, dcm_controller.dcmDes)
+
+dcm_controller.init(dt)
+
+robot.dcm_control = dcm_controller
+
+Ki_dcm = [1.0,1.0,1.0] # this value is employed later
+
+# --- CoM admittance controller
+Kp_adm = [0.0,0.0,0.0] # zero (to be set later)
+
+com_admittance_control = ComAdmittanceController("comAdmCtrl")
+com_admittance_control.Kp.value = Kp_adm
+plug(robot.zmp_estimator.zmp,com_admittance_control.zmp)
+com_admittance_control.zmpDes.value = robot.wp.zmpDes.value     # should be plugged to robot.dcm_control.zmpRef
+plug(robot.wp.acomDes, com_admittance_control.ddcomDes)
+
+com_admittance_control.init(dt)
+com_admittance_control.setState(robot.wp.comDes.value,[0.0,0.0,0.0])
+
+robot.com_admittance_control = com_admittance_control
+
+Kp_adm = [12.0,12.0,0.0] # this value is employed later
+
+# --- Control Manager
+robot.cm = create_ctrl_manager(cm_conf, dt, robot_name='robot')
+robot.cm.addCtrlMode('sot_input')
+robot.cm.setCtrlMode('all','sot_input')
+robot.cm.addEmergencyStopSIN('zmp')
+
+# -------------------------- SOT CONTROL --------------------------
+
+# --- Upper body
+robot.taskUpperBody = Task ('task_upper_body')
+robot.taskUpperBody.feature = FeaturePosture('feature_upper_body')
+
+q = list(robot.dynamic.position.value)
+robot.taskUpperBody.feature.state.value = q
+robot.taskUpperBody.feature.posture.value = q
+
+# robotDim = robot.dynamic.getDimension() # 38
+robot.taskUpperBody.feature.selectDof(18,True)
+robot.taskUpperBody.feature.selectDof(19,True)
+robot.taskUpperBody.feature.selectDof(20,True)
+robot.taskUpperBody.feature.selectDof(21,True)
+robot.taskUpperBody.feature.selectDof(22,True)
+robot.taskUpperBody.feature.selectDof(23,True)
+robot.taskUpperBody.feature.selectDof(24,True)
+robot.taskUpperBody.feature.selectDof(25,True)
+robot.taskUpperBody.feature.selectDof(26,True)
+robot.taskUpperBody.feature.selectDof(27,True)
+robot.taskUpperBody.feature.selectDof(28,True)
+robot.taskUpperBody.feature.selectDof(29,True)
+robot.taskUpperBody.feature.selectDof(30,True)
+robot.taskUpperBody.feature.selectDof(31,True)
+robot.taskUpperBody.feature.selectDof(32,True)
+robot.taskUpperBody.feature.selectDof(33,True)
+robot.taskUpperBody.feature.selectDof(34,True)
+robot.taskUpperBody.feature.selectDof(35,True)
+robot.taskUpperBody.feature.selectDof(36,True)
+robot.taskUpperBody.feature.selectDof(37,True)
+
+robot.taskUpperBody.controlGain.value = 100.0
+robot.taskUpperBody.add(robot.taskUpperBody.feature.name)
+plug(robot.dynamic.position, robot.taskUpperBody.feature.state)
+
+# --- CONTACTS
+#define contactLF and contactRF
+robot.contactLF = MetaTaskKine6d('contactLF',robot.dynamic,'LF',robot.OperationalPointsMap['left-ankle'])
+robot.contactLF.feature.frame('desired')
+robot.contactLF.gain.setConstant(300)
+plug(robot.wp.footLeftDes, robot.contactLF.featureDes.position) #.errorIN?
+locals()['contactLF'] = robot.contactLF
+
+robot.contactRF = MetaTaskKine6d('contactRF',robot.dynamic,'RF',robot.OperationalPointsMap['right-ankle'])
+robot.contactRF.feature.frame('desired')
+robot.contactRF.gain.setConstant(300)
+plug(robot.wp.footRightDes, robot.contactRF.featureDes.position) #.errorIN?
+locals()['contactRF'] = robot.contactRF
+
+# --- COM
+robot.taskCom = MetaTaskKineCom(robot.dynamic)
+plug(robot.com_admittance_control.comRef,robot.taskCom.featureDes.errorIN)
+plug(robot.com_admittance_control.dcomRef,robot.taskCom.featureDes.errordotIN)
+robot.taskCom.task.controlGain.value = 0
+robot.taskCom.task.setWithDerivative(True)
+
+# --- Waist
+robot.keepWaist = MetaTaskKine6d('keepWaist',robot.dynamic,'WT',robot.OperationalPointsMap['waist'])
+robot.keepWaist.feature.frame('desired')
+robot.keepWaist.gain.setConstant(300)
+plug(robot.wp.waistDes, robot.keepWaist.featureDes.position)
+robot.keepWaist.feature.selec.value = '111000'
+locals()['keepWaist'] = robot.keepWaist
+
+# # --- Posture
+# robot.taskPos = Task ('task_pos')
+# robot.taskPos.feature = FeaturePosture('feature_pos')
+#
+# q = list(robot.dynamic.position.value)
+# robot.taskPos.feature.state.value = q
+# robot.taskPos.feature.posture.value = q
+
+# robotDim = robot.dynamic.getDimension() # 38
+#robot.taskPos.feature.selectDof(6,True)
+#robot.taskPos.feature.selectDof(7,True)
+#robot.taskPos.feature.selectDof(8,True)
+#robot.taskPos.feature.selectDof(9,True)
+#robot.taskPos.feature.selectDof(10,True)
+#robot.taskPos.feature.selectDof(11,True)
+#robot.taskPos.feature.selectDof(12,True)
+#robot.taskPos.feature.selectDof(13,True)
+#robot.taskPos.feature.selectDof(14,True)
+#robot.taskPos.feature.selectDof(15,True)
+#robot.taskPos.feature.selectDof(16,True)
+#robot.taskPos.feature.selectDof(17,True)
+#robot.taskPos.feature.selectDof(18,True)
+#robot.taskPos.feature.selectDof(19,True)
+#robot.taskPos.feature.selectDof(20,True)
+#robot.taskPos.feature.selectDof(21,True)
+#robot.taskPos.feature.selectDof(22,True)
+#robot.taskPos.feature.selectDof(23,True)
+#robot.taskPos.feature.selectDof(24,True)
+#robot.taskPos.feature.selectDof(25,True)
+#robot.taskPos.feature.selectDof(26,True)
+#robot.taskPos.feature.selectDof(27,True)
+#robot.taskPos.feature.selectDof(28,True)
+#robot.taskPos.feature.selectDof(29,True)
+#robot.taskPos.feature.selectDof(30,True)
+#robot.taskPos.feature.selectDof(31,True)
+#robot.taskPos.feature.selectDof(32,True)
+#robot.taskPos.feature.selectDof(33,True)
+#robot.taskPos.feature.selectDof(34,True)
+#robot.taskPos.feature.selectDof(35,True)
+#robot.taskPos.feature.selectDof(36,True)
+#robot.taskPos.feature.selectDof(37,True)
+
+#robot.taskPos.controlGain.value = 100.0
+#robot.taskPos.add(robot.taskPos.feature.name)
+#plug(robot.dynamic.position, robot.taskPos.feature.state)
+
+# --- SOT solver
+robot.sot = SOT('sot')
+robot.sot.setSize(robot.dynamic.getDimension())
+
+# --- Plug SOT control to device through control manager
+plug(robot.sot.control,robot.cm.ctrl_sot_input)
+plug(robot.cm.u_safe,robot.device.control)
+
+robot.sot.push(robot.taskUpperBody.name)
+robot.sot.push(robot.contactRF.task.name)
+robot.sot.push(robot.contactLF.task.name)
+robot.sot.push(robot.taskCom.task.name)
+robot.sot.push(robot.keepWaist.task.name)
+# robot.sot.push(robot.taskPos.name)
+# robot.device.control.recompute(0) # this crashes as it employs joint sensors which are not ready yet
+
+# --- Fix robot.dynamic inputs
+plug(robot.device.velocity,robot.dynamic.velocity)
+from dynamic_graph.sot.core import Derivator_of_Vector
+robot.dvdt = Derivator_of_Vector("dv_dt")
+robot.dvdt.dt.value = dt
+plug(robot.device.velocity,robot.dvdt.sin)
+plug(robot.dvdt.sout,robot.dynamic.acceleration)
+
+# -------------------------- PLOTS --------------------------
+
+# --- ROS PUBLISHER
+robot.publisher = create_rospublish(robot, 'robot_publisher')        
+
+create_topic(robot.publisher, robot.wp, 'comDes', robot = robot, data_type='vector')                      # desired CoM
+
+create_topic(robot.publisher, robot.cdc_estimator, 'c', robot = robot, data_type='vector')                # estimated CoM
+create_topic(robot.publisher, robot.cdc_estimator, 'dc', robot = robot, data_type='vector')               # estimated CoM velocity
+
+create_topic(robot.publisher, robot.com_admittance_control, 'comRef', robot = robot, data_type='vector')  # reference CoM
+create_topic(robot.publisher, robot.dynamic, 'com', robot = robot, data_type='vector')                    # resulting SOT CoM
+
+create_topic(robot.publisher, robot.dcm_control, 'dcmDes', robot = robot, data_type='vector')             # desired DCM
+create_topic(robot.publisher, robot.estimator, 'dcm', robot = robot, data_type='vector')                  # estimated DCM
+
+create_topic(robot.publisher, robot.dcm_control, 'zmpDes', robot = robot, data_type='vector')             # desired ZMP
+create_topic(robot.publisher, robot.dynamic, 'zmp', robot = robot, data_type='vector')                    # SOT ZMP
+create_topic(robot.publisher, robot.zmp_estimator, 'zmp', robot = robot, data_type='vector')              # estimated ZMP
+create_topic(robot.publisher, robot.dcm_control, 'zmpRef', robot = robot, data_type='vector')             # reference ZMP
+
+#create_topic(robot.publisher, robot.device, 'forceLLEG', robot = robot, data_type='vector')               # measured left wrench
+#create_topic(robot.publisher, robot.device, 'forceRLEG', robot = robot, data_type='vector')               # measured right wrench
+
+#create_topic(robot.publisher, robot.device_filters.ft_LF_filter, 'x_filtered', robot = robot, data_type='vector') # filtered left wrench
+#create_topic(robot.publisher, robot.device_filters.ft_RF_filter, 'x_filtered', robot = robot, data_type='vector') # filtered right wrench
+
+create_topic(robot.publisher, robot.ftc, 'left_foot_force_out', robot = robot, data_type='vector')  # calibrated left wrench
+create_topic(robot.publisher, robot.ftc, 'right_foot_force_out', robot = robot, data_type='vector') # calibrated right wrench
+
+# --- TRACER
+robot.tracer = TracerRealTime("com_tracer")
+robot.tracer.setBufferSize(80*(2**20))
+robot.tracer.open('/tmp','dg_','.dat')
+robot.device.after.addSignal('{0}.triger'.format(robot.tracer.name))
+
+addTrace(robot.tracer, robot.wp, 'comDes')                      # desired CoM
+
+addTrace(robot.tracer, robot.cdc_estimator, 'c')                # estimated CoM
+addTrace(robot.tracer, robot.cdc_estimator, 'dc')               # estimated CoM velocity
+
+addTrace(robot.tracer, robot.com_admittance_control, 'comRef')  # reference CoM
+addTrace(robot.tracer, robot.dynamic, 'com')                    # resulting SOT CoM
+
+addTrace(robot.tracer, robot.dcm_control, 'dcmDes')             # desired DCM
+addTrace(robot.tracer, robot.estimator, 'dcm')                  # estimated DCM
+
+addTrace(robot.tracer, robot.dcm_control, 'zmpDes')             # desired ZMP
+addTrace(robot.tracer, robot.dynamic, 'zmp')                    # SOT ZMP
+addTrace(robot.tracer, robot.zmp_estimator, 'zmp')              # estimated ZMP
+addTrace(robot.tracer, robot.dcm_control, 'zmpRef')             # reference ZMP
+
+addTrace(robot.tracer, robot.ftc, 'left_foot_force_out')        # calibrated left wrench
+addTrace(robot.tracer,  robot.ftc, 'right_foot_force_out')      # calibrated right wrench
+
+robot.tracer.start()
+

python/sot_talos_balance/test/test_dcmZmpControl_file.py
@@ -1,0 +1,32 @@
+'''Test CoM admittance control as described in paper, with pre-loaded movements'''
+from sot_talos_balance.utils.run_test_utils import *
+from time import sleep
+
+run_test('appli_dcmZmpControl_file.py')
+
+run_ft_calibration('robot.ftc')
+raw_input("Wait before running the test")
+
+# Connect ZMP reference and reset controllers
+print('Connect ZMP reference')
+runCommandClient('plug(robot.zmp_estimator.emergencyStop,robot.cm.emergencyStop_zmp)')
+runCommandClient('plug(robot.dcm_control.zmpRef,robot.com_admittance_control.zmpDes)')
+runCommandClient('robot.com_admittance_control.setState(robot.wp.comDes.value,[0.0,0.0,0.0])')
+runCommandClient('robot.com_admittance_control.Kp.value = Kp_adm')
+runCommandClient('robot.dcm_control.resetDcmIntegralError()')
+runCommandClient('robot.dcm_control.Ki.value = Ki_dcm')
+
+# Play the movements
+c = ask_for_confirmation("Execute a sinusoid?")
+if c:
+    print("Executing the sinusoid...")
+    runCommandClient('robot.comTrajGen.move(1,-0.025,1.0)')
+    runCommandClient('robot.zmpTrajGen.move(1,-0.025,1.0)')
+    sleep(5.0)
+    runCommandClient('robot.comTrajGen.startSinusoid(1,0.025,2.0)')
+    runCommandClient('robot.zmpTrajGen.startSinusoid(1,0.025,2.0)')
+
+raw_input("Wait before dumping the data")
+
+runCommandClient('dump_tracer(robot.tracer)')
+

python/sot_talos_balance/utils/run_test_utils.py
@@ -108,6 +108,7 @@ def run_ft_calibration(sensor_name,force=False):
         runCommandClient(sensor_name+'.calibrateFeetSensor()')
         sleep(1.0) # TODO: get time/state from F/T sensor
         print("Sensors are calibrated!")
+        print("You can now put the robot on the ground")
     else:
         print("Skipping sensor calibration")
 

commit 6e20c3793d9e245d975ca6a7171ea542f3c57d57

[wpgen] Fix bug introduced by 37db1274

src/dummy-walking-pattern-generator.cpp
@@ -225,7 +225,7 @@ namespace dynamicgraph
           const Vector & comDes = m_comDesSOUT(iter);
           const Vector & acomDes = m_acomDesSOUT(iter);
 
-          Vector zmpDes = comDes - acomDes/(omega*omega);
+          zmpDes = comDes - acomDes/(omega*omega);
           zmpDes[2] = 0.0;
         }
 