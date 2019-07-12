2211b51b21743f0b630d7c80fb66652f07ade2a3
Fanny Risbourg <frisbourg@laas.fr>
2019-07-12 10:30:11 +0200

    [Coupled Ankle Admittance] Test cleaned\

--- python/sot_talos_balance/test/test_dcmCoupledAnkleControl.py
+++ python/sot_talos_balance/test/test_dcmCoupledAnkleControl.py
@@ -20,7 +20,6 @@ runCommandClient('robot.rollController.kSum.value = [0.0]')
 runCommandClient('robot.rollController.kDiff.value =  [0.0]')
 runCommandClient('robot.dcm_control.resetDcmIntegralError()')
 runCommandClient('robot.dcm_control.Ki.value = Ki_dcm')
-# runCommandClient('robot.dcm_control.Kp.value = []')
 
 c = ask_for_confirmation("Execute a sinusoid?")
 if c:
@@ -51,55 +50,4 @@ if c:
 else:
     print("Not executing the sinusoid")
 
-c = ask_for_confirmation("Raise the foot?")
-if c:
-    print("Putting the robot in position...")
-    runCommandClient('robot.comTrajGen.move(1,-0.08,10.0)')
-    runCommandClient('robot.rhoTrajGen.move(0,0.4,10.0)')
-    sleep(10.0)
-    print("Robot is in position!")
-
-    c2 = ask_for_confirmation("Confirm raising the foot?")
-    if c2:
-        print("Raising the foot...")
-        runCommandClient('robot.leftAnklePitchTask.task.controlSelec.value="0"*robot.sot.getSize()')
-        runCommandClient('robot.leftAnkleRollTask.task.controlSelec.value="0"*robot.sot.getSize()')
-        runCommandClient('robot.distribute.phase.value = -1')
-        runCommandClient('h = robot.dynamic.LF.value[2][3]')
-        runCommandClient('robot.lfTrajGen.move(2,h+0.05,10.0)')
-        sleep(10.0)
-        print("Foot has been raised!")
-        c3 = ask_for_confirmation("Put the foot back?")
-    else:
-        print("Not raising the foot")
-        c3 = False
-
-    if c3:
-        print("Putting the foot back...")
-        runCommandClient('robot.lfTrajGen.move(2,h,10.0)')
-        sleep(10.0)
-        runCommandClient('robot.distribute.phase.value = 0')
-        runCommandClient('robot.leftAnklePitchTask.task.controlSelec.value="1"*robot.sot.getSize()')
-        runCommandClient('robot.leftAnkleRollTask.task.controlSelec.value="1"*robot.sot.getSize()')
-        print("The foot is back in position!")
-    else:
-        print("Not putting the foot back")
-
-    if c3 or not c2:
-        c4 = ask_for_confirmation("Put the robot back?")
-    else:
-        c4 = False
-
-    if c4:
-        print("Putting the robot back...")
-        runCommandClient('robot.comTrajGen.move(1,0.0,10.0)')
-        runCommandClient('robot.rhoTrajGen.move(0,0.5,10.0)')
-        sleep(10.0)
-        print("The robot is back in position!")
-else:
-    print("Not raising the foot")
-
-#raw_input("Wait before dumping the data")
-
-#runCommandClient('dump_tracer(robot.tracer)')
-
+raw_input("Wait before ending the test")
