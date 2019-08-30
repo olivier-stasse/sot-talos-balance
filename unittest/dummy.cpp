0d85cca2d123b936442dde86ce9de93fdc009f4f
Gabriele Buondonno <gbuondon@laas.fr>
2019-08-30 18:19:46 +0200

    [unittest] Add dummy unit test

---
+++ unittest/dummy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/utility/binary.hpp>
+
+BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
+
+BOOST_AUTO_TEST_CASE ( test_dummy )
+{
+  BOOST_CHECK(true);
+  std::cout << "It works!" << std::endl;
+} 
+
+BOOST_AUTO_TEST_SUITE_END ()
