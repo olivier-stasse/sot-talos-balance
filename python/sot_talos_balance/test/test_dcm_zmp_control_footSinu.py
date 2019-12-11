2462d34ae3c8b45eb80ae0f3d4085971fd844b52
NoelieRamuzat <noelie.ramuzat@laas.fr>
2019-12-11 11:03:04 +0100

    [test_footSinusoid] Add sleep to avoid robot falling + slower the motion

--- python/sot_talos_balance/test/test_dcm_zmp_control_footSinu.py
+++ python/sot_talos_balance/test/test_dcm_zmp_control_footSinu.py
@@ -4,34 +4,28 @@ from time import sleep
 
 from sys import argv
 
-test_folder = None
-run_test('appli_dcm_zmp_control_ffdc.py')
+runCommandClient('test_folder = None')
+run_test('appli_dcm_zmp_control.py')
 
 run_ft_calibration('robot.ftc')
 
 raw_input("Wait before running the test")
 
 # Connect ZMP reference and reset controllers
-print('Set controller')
+print('Connect ZMP reference')
 runCommandClient('plug(robot.zmp_estimator.emergencyStop,robot.cm.emergencyStop_zmp)')
-runCommandClient('plug(robot.distribute.emergencyStop,robot.cm.emergencyStop_distribute)')
-runCommandClient('plug(robot.distribute.zmpRef,robot.com_admittance_control.zmpDes)')
+runCommandClient('plug(robot.dcm_control.zmpRef,robot.com_admittance_control.zmpDes)')
 runCommandClient('robot.com_admittance_control.setState(robot.wp.comDes.value,[0.0,0.0,0.0])')
 runCommandClient('robot.com_admittance_control.Kp.value = Kp_adm')
 runCommandClient('robot.dcm_control.resetDcmIntegralError()')
 runCommandClient('robot.dcm_control.Ki.value = Ki_dcm')
-
-raw_input("Wait before activating foot force difference control")
-runCommandClient('robot.ffdc.dfzAdmittance.value = dfzAdmittance')
-runCommandClient('robot.ffdc.vdcFrequency.value = vdcFrequency')
-runCommandClient('robot.ffdc.vdcDamping.value = vdcDamping')
+runCommandClient('robot.dcm_control.Kz.value = Kz_dcm')
 
 
 c = ask_for_confirmation("Raise the foot?")
 if c:
     print("Putting the robot in position...")
     runCommandClient('robot.comTrajGen.move(1,-0.07,10.0)')
-    runCommandClient('robot.rhoTrajGen.move(0,0.4,10.0)')
     sleep(10.0)
     print("Robot is in position!")
 
@@ -40,7 +34,6 @@ if c:
     c2 = ask_for_confirmation("Confirm raising the foot?")
     if c2:
         print("Raising the foot...")
-        runCommandClient('robot.phaseTrajGen.set(0,-1)')
         runCommandClient('h = robot.dynamic.LF.value[2][3]')
         runCommandClient('robot.lfTrajGen.move(2,h+0.05,10.0)')
         sleep(10.0)
@@ -56,8 +49,8 @@ if c:
             c6 = ask_for_confirmation("Start sinusoid move?")
             if c6:
                 print("Start sinusoid move...")
-                runCommandClient('robot.lfTrajGen.startSinusoid(0,0.1,4.0)')
-                runCommandClient('robot.lfTrajGen.startSinusoid(2,h+0.05,2.0)')
+                runCommandClient('robot.lfTrajGen.startSinusoid(0,0.1,6.0)')
+                runCommandClient('robot.lfTrajGen.startSinusoid(2,h+0.05,3.0)')
                 print("Sinusoid started!")
             else:
                 print("Not executing the sinusoid")
@@ -65,14 +58,16 @@ if c:
             print("Not Moving to sinusoid")
         c4 = ask_for_confirmation("Put the foot back?")
         if c4:
-            print("Putting the foot back...")
+            print("Stopping the robot...")
             runCommandClient('robot.lfTrajGen.stop(0)')
-            runCommandClient('robot.lfTrajGen.stop(2)')            
+            sleep(5.0)
+            runCommandClient('robot.lfTrajGen.stop(2)')  
+            sleep(5.0)   
+            print("Putting the robot back...")       
             runCommandClient('robot.lfTrajGen.move(0,0,10.0)')
-            sleep(10.0)
+            sleep(15.0)
             runCommandClient('robot.lfTrajGen.move(2,h,10.0)')
-            sleep(10.0)
-            runCommandClient('robot.phaseTrajGen.set(0,0)')
+            sleep(15.0)
             print("The foot is back in position!")
             foot_on_ground = True
         else:
@@ -85,7 +80,6 @@ if c:
         if c5:
             print("Putting the robot back...")
             runCommandClient('robot.comTrajGen.move(1,0.0,10.0)')
-            runCommandClient('robot.rhoTrajGen.move(0,0.5,10.0)')
             sleep(10.0)
             print("The robot is back in position!")
         else:
